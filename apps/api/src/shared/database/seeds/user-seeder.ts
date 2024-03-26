7264b0df4d8d36e2d26b7ef0cc0f76564a51dec2
Petar Trutanic <ptrutanic@extensionengine.com>
2024-03-26 11:47:37 +0100

    Updated user seed

--- apps/api/src/shared/database/seeds/user-seeder.ts
+++ apps/api/src/shared/database/seeds/user-seeder.ts
@@ -1,22 +1,59 @@
 import type { Dictionary, EntityManager } from '@mikro-orm/postgresql';
 import { Seeder } from '@mikro-orm/seeder';
 import { createUser } from '../factories/user.factory';
+import {
+  getAll,
+  create,
+  IdentityProviderUser,
+} from '../factories/identity-provider.factory';
+
+const SEED_EMAIL_DOMAIN = 'example.org';
+const SEED_PASSWORD = process.env.DB_SEED_USER_PASSWORD;
 
 export class UserSeeder extends Seeder {
   async run(em: EntityManager, context: Dictionary): Promise<void> {
+    const identityProviderUsers = await getAll(`email:*${SEED_EMAIL_DOMAIN}`);
+
+    const adminEmail = `admin@${SEED_EMAIL_DOMAIN}`;
     const pAdmin = createUser(em, {
-      email: 'admin@example.org',
+      email: adminEmail,
+      identityProviderId: await this.getIdentityProviderId(
+        adminEmail,
+        identityProviderUsers,
+      ),
       role: 'admin',
     });
-    const pUsers = [...Array(10)].map((_, index) => {
-      const email = index > 0 ? `user${index}@example.org` : 'user@example.org';
+
+    const pUsers = [...Array(10)].map(async (_, index) => {
+      const email =
+        index > 0
+          ? `user${index}@${SEED_EMAIL_DOMAIN}`
+          : `user@${SEED_EMAIL_DOMAIN}`;
+
+      const identityProviderId = await this.getIdentityProviderId(
+        email,
+        identityProviderUsers,
+      );
+
       return createUser(em, {
         email,
+        identityProviderId,
         role: 'user',
       });
     });
 
     const users = await Promise.all([pAdmin, ...pUsers]);
     context.userIds = users;
+  }
+
+  private async getIdentityProviderId(
+    email: string,
+    identityProviderUsers: IdentityProviderUser[],
+  ) {
+    const existingUserId = identityProviderUsers.find(
+      (user) => user.email === email,
+    )?.user_id;
+
+    return existingUserId ?? (await create(email, SEED_PASSWORD));
   }
 }
